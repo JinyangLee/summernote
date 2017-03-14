1f47897c34406517aeeafe0e1ed8ebf353a92a8c
lijinyang <lithium@lijinyangs-iMac.local>
2017-03-14 10:49:29 +0800

    [UPDATE] intro

--- src/js/intro.js
+++ src/js/intro.js
@@ -9,16 +9,17 @@
  * Date: @DATE
  */
 (function (factory) {
-  /* global define */
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define(['jquery'], factory);
-  } else if (typeof module === 'object' && module.exports) {
-    // Node/CommonJS
-    module.exports = factory(require('jquery'));
-  } else {
-    // Browser globals
-    factory(window.jQuery);
-  }
+  // /* global define */
+  // if (typeof define === 'function' && define.amd) {
+  //   // AMD. Register as an anonymous module.
+  //   define(['jquery'], factory);
+  // } else if (typeof module === 'object' && module.exports) {
+  //   // Node/CommonJS
+  //   module.exports = factory(require('jquery'));
+  // } else {
+  //   // Browser globals
+  //   factory(window.jQuery);
+  // }
+  factory(window.jQuery);
 }(function ($) {
   'use strict';
